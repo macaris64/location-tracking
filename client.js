feaa029c2967aa497f6bdaacce4eb24d17b19f29
Mehmet Acar <mehmetacar683@gmail.com>
2019-11-23 18:29:13 +0300

    retain messages

--- client.js
+++ client.js
@@ -1,15 +1,18 @@
 var mqtt = require('mqtt');
 var client = mqtt.connect('mqtt://127.0.0.1:1883');
 
-client.subscribe('new-user');
 client.subscribe('location');
 
-client.on('connect', () => {
-    console.log('a client has been connected');
+client.on('connect', function() {
+    console.log('connected!');
 
-    client.publish('new-user','Mehmet-' + Math.ceil(Math.random() * 10));
+    client.subscribe('new-user', function() {
+        client.publish('new-user', 'Mehmet-' + Math.ceil(Math.random() * 10), {
+            retain: true,
+        });
+    });
 });
 
-client.on('message', (topic, message) => {
-    console.log(topic, ' : ', message.toString())
+client.on('message', function(topic, message) {
+    console.log(topic, ' : ', message.toString());
 });
